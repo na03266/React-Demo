468246a587c35184615cee61d4a6ace0d2039890
NaHwangje <na03266@naver.com>
2023-08-28 10:35:40 +0900

    버튼 이벤트 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 function App() {
   const [names, setName] = useState(['홍길동', '김민수']);
@@ -8,17 +8,21 @@ function App() {
     setInput(e.target.value);
   };
 
-  
+  const handleUpload = () => {
+    setName((prevState) => {
+      return ([input, ...prevState])
+    })
+  };
 
-  console.log('업데이트!!');
+  console.log(input);
 
   return (
     <div>
-      <input type='text' value={input} onChange={handleInputChange}></input>
-            <button onClick={handleInputChange}>Update</button>
-            {names.map((name, idx) =>{
-              return <p key={idx}>{name}</p>
-            })}
+      <input type='text' value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}>Update</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>
+      })}
     </div>
 
   );
