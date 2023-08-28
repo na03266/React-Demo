9cd10953908a08971244608e3b065bb1cceb201d
NaHwangje <na03266@naver.com>
2023-08-28 09:18:30 +0900

    참고 자료 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import TodoListTemplate from './components/TodoListTemplate';
+import React from 'react';
+import logo from '.logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <TodoListTemplate>
-        템플릿 완성
-      </TodoListTemplate>
-    );
-  }
+function App(){
+  return(
+        <div className='App'>
+        dsdasdasdf
+        </div>
+        
+  )
 }
 
 export default App;
