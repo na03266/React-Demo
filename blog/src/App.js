86f6a8b187f5d252c5a01e407f10f817632ec1c9
NaHwangje <na03266@naver.com>
2023-08-28 11:42:27 +0900

    타이머 코드는 완성했으나, 버튼 클릭시 서버 오류 발생2

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,34 +1,14 @@
-import { useState } from 'react'
+import React, {useState, useEffect} from "react";
+import Timer from './components/Timer';
 
-const heavyWork = () => {
-  console.log('엄청 무거운 작업');
-  return ['홍길동', '김민수'];
-};
 
 function App() {
-  const [names, setName] = useState(()=>{
-    return heavyWork();
-  });
-  const [input, setInput] = useState('');
-
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  const handleUpload = () => {
-    setName((prevState) => {
-      return ([input, ...prevState]);
-    })
-  };
-
+  const [showTimer, setShowTimer] = useState(false);
 
   return (
     <div>
-      <input type='text' value={input} onChange={handleInputChange} />
-      <button onClick={handleUpload}>Update</button>
-      {names.map((name, idx) => {
-        return <p key={idx}>{name}</p>
-      })}
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle timer</button>
     </div>
 
   );
